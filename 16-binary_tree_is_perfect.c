37f8afd22bf0212a926b480a2b149cf825009349
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-07-18 21:20:09 +0000

    fixed 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,22 +1,31 @@
 #include "binary_trees.h"
 /**
-*binary_tree_depth -  function that measures the depth of a binary tree
+*binary_tree_size -  function that measures the size of a binary tree
 *@tree: tree schema
+*Return: return
 **/
-size_t binary_tree_depth(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-    if (!tree->parent)
-        return (0);
-    return (1 +  binary_tree_depth(tree->parent));
+	int a = 0;
+	int b = 0;
+
+	if (!tree)
+		return (0);
+	a = (binary_tree_size(tree->left));
+	b = (binary_tree_size(tree->right));
+	return (a + b + 1);
 }
 /**
 *binary_tree_is_perfect -  function that measures the leaves of a binary tree
 *@tree: tree schema
+*Return: int
 **/
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    int d = binary_tree_depth(tree);
-    return is_perfect(tree, d, 0);
+	if (!tree)
+		return (0);
+	if (binary_tree_size(tree->left) == binary_tree_size(tree->right))
+		return (1);
+	else
+		return (0);
 }
