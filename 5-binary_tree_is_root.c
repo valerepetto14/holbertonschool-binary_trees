4bfefa821d3009e799d0e25fc6d6c0cfc05630ff
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-07-18 20:28:31 +0000

    fixed 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,8 +9,8 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (!node)
 		return (0);
-	if (node->left == NULL && node->right == NULL)
-		return (0);
-	else
+	if (node->parent == NULL)
 		return (1);
+	else
+		return (0);
 }
