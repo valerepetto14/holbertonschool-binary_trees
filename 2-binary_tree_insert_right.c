f2f2a65ff8f6aecadc9666a8a08de7e918172f0d
tarikaudi <tarik.audi@hotmail.com>
2022-07-18 11:03:40 -0700

    betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,32 +1,35 @@
 #include "binary_trees.h"
 /**
-*binary_tree_insert_left - function
+*binary_tree_insert_right - function
 *@parent: padre
 *@value: value
+*Return: return
 **/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t * newnode = NULL;
-    binary_tree_t * aux = NULL;
+	binary_tree_t *newnode = NULL;
+	binary_tree_t *aux = NULL;
 
-    if (!parent)
-        return (NULL);
-    newnode = malloc(sizeof(binary_tree_t));
-    if (!newnode)
-        return (NULL);
-    newnode->n = value;
-    newnode->left = NULL;
-    newnode->right = NULL;
-    newnode->parent = parent;
-    if(parent->right){
-        aux = parent->right;
-        aux->parent = newnode;
-        newnode->right = aux;
-        parent->right = newnode;
-        return (newnode);
-    }
-    else{
-        parent->right = newnode;
-        return (newnode);
-    }
+	if (!parent)
+		return (NULL);
+	newnode = malloc(sizeof(binary_tree_t));
+	if (!newnode)
+		return (NULL);
+	newnode->n = value;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	newnode->parent = parent;
+	if (parent->right)
+	{
+		aux = parent->right;
+		aux->parent = newnode;
+		newnode->right = aux;
+		parent->right = newnode;
+	return (newnode);
+	}
+	else
+	{
+		parent->right = newnode;
+		return (newnode);
+	}
 }
