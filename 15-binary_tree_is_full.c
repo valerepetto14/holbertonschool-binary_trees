4d29670714c88976b3c41c37828b646a5f3d7f57
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-07-18 20:52:31 +0000

    fixed 5

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,12 +7,13 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (1);
+		return (0);
 
 	if (!tree->left && !tree->right)
 		return (1);
 
 	else if (tree->left && tree->right)
 		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
 	return (0);
 }
