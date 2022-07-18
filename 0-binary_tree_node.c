ba5aa3b2441873f393b00c84fb1c33d4b6a41119
tarikaudi <tarik.audi@hotmail.com>
2022-07-18 10:45:12 -0700

    betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,15 +2,18 @@
 /**
 *binary_tree_node - function
 *@parent: padre
-*@value: value
+*@value: valuei
+*Return: return
 **/
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newnode = NULL;
-    newnode = malloc(sizeof(binary_tree_t));
-    if(!newnode)
-        return (NULL);
-    newnode->parent = parent;
-    newnode->n = value;
-    return (newnode);
-}   
+	binary_tree_t *newnode = NULL;
+
+	newnode = malloc(sizeof(binary_tree_t));
+	if (!newnode)
+		return (NULL);
+	newnode->parent = parent;
+	newnode->n = value;
+	return (newnode);
+}
