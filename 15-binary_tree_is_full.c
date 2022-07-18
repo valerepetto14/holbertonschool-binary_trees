faf249e35ef0e553938edbe721a4869cbb5fcdbd
tarikaudi <tarik.audi@hotmail.com>
2022-07-18 11:58:09 -0700

    betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,17 +1,18 @@
 #include "binary_trees.h"
 /**
-*binary_tree_balance -  function that measures the leaves of a binary tree
+*binary_tree_is_full -  function that measures the leaves of a binary tree
 *@tree: tree schema
+*Return: return
 **/
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-      return (1);
+	if (tree == NULL)
+		return (1);
 
-    if (!tree->left && !tree->right)
-        return (1);
+	if (!tree->left && !tree->right)
+		return (1);
 
-    else if (tree->left && tree->right)
-        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-    return (0);
+	else if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
 }
