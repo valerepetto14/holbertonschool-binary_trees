3350740eca5c88969845ae6276abf9d0dc7a269d
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-07-18 20:23:43 +0000

    fixed 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,8 +8,8 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (!node)
 		return (0);
-	if (node->left != NULL && node->right != NULL)
-		return (0);
-	else
+	if (node->left == NULL && node->right == NULL)
 		return (1);
+	else
+		return (0);
 }
