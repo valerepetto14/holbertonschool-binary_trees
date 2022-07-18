4872b80636b2319d9db2894a02cb316a89dff15b
tarikaudi <tarik.audi@hotmail.com>
2022-07-18 11:05:31 -0700

    free

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 /**
 *binary_tree_delete - function that delete a tree binary
-*@tree: tree schema 
+*@tree: tree schema
 **/
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (!tree)
-        return;
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
-    free(tree);
+	if (!tree)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
