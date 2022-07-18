08fc9c80c51064dd5ac57933208e8a58b8628881
tarikaudi <tarik.audi@hotmail.com>
2022-07-18 11:44:44 -0700

    betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -2,16 +2,16 @@
 /**
 *binary_tree_nodes -  function that measures the leaves of a binary tree
 *@tree: tree schema
+*Return: return
 **/
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (!tree)
-        return 0;
-    else if (!tree->left && tree->right)
-        return (1 + binary_tree_nodes(tree->right));
-    else if (tree->left && !tree->right)
-        return (1 + binary_tree_nodes(tree->left));
-    else
-        return binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left);
-
+	if (!tree)
+		return 0;
+	else if (!tree->left && tree->right)
+		return (1 + binary_tree_nodes(tree->right));
+	else if (tree->left && !tree->right)
+		return (1 + binary_tree_nodes(tree->left));
+	else
+		return (binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left));
 }
