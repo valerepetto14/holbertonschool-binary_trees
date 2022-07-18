65527179efb379d050480ef09341be0fb18d994e
tarikaudi <tarik.audi@hotmail.com>
2022-07-18 11:23:35 -0700

    betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,19 +2,21 @@
 /**
 *binary_tree_height -  function that measures the height of a binary tree
 *@tree: tree schema
+*Return: return
 **/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    int a = 0;
-    int b = 0;
-    if (!tree)
-        return (0);
-    if(!tree->left && !tree->right)
-        return (0);
-    a = (1 + binary_tree_height(tree->left));
-    b = (1 + binary_tree_height(tree->right));
-    if (a > b)
-        return (a);
-    else
-        return (b);
+	int a = 0;
+	int b = 0;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
+	a = (1 + binary_tree_height(tree->left));
+	b = (1 + binary_tree_height(tree->right));
+	if (a > b)
+		return (a);
+	else
+		return (b);
 }
