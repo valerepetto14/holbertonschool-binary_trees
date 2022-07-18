43fdec6918f6419db825556807417b642cf27935
tarikaudi <tarik.audi@hotmail.com>
2022-07-18 11:30:40 -0700

    betty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,14 +2,16 @@
 /**
 *binary_tree_size -  function that measures the size of a binary tree
 *@tree: tree schema
+*Return: return
 **/
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    int a = 0;
-    int b = 0;
-    if (!tree)
-        return (0);
-    a = (binary_tree_size(tree->left));
-    b = (binary_tree_size(tree->right));
-    return (a + b + 1);
+	int a = 0;
+	int b = 0;
+
+	if (!tree)
+		return (0);
+	a = (binary_tree_size(tree->left));
+	b = (binary_tree_size(tree->right));
+	return (a + b + 1);
 }
